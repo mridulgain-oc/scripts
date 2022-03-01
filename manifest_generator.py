823163534b70d1dc7fdcd3dd705ccd7bdbe91979
Mridul Gain <78899625+mridulgain-oc@users.noreply.github.com>
2022-03-01 11:52:53 +0530

    Update manifest_generator.py

--- manifest_generator.py
+++ manifest_generator.py
@@ -1,9 +1,11 @@
 import os, yaml
 
-RESOURCE_SET=300 # each set contains a namespace, an snatpolicy, an ngnix deployment & a service
+RESOURCE_SET=2 # each set contains a namespace, an snatpolicy, an ngnix deployment & a service
 REPLICAS=1 # no of pods per deployment or, no of endpoints per service
 # OUTPUT_PATH = './yamls'
 OUTPUT_FILE = 'resources.yaml'
+IDENTIFIER_PREFIX = 'dummy-'
+IP_PREFIX = '151.20.1.'
 MANIFEST_BASE = '''
 ---
 apiVersion: v1
@@ -68,33 +70,38 @@ for i in range(1, RESOURCE_SET+1):
     yamls = list(yamls) # generator to list
 
     # ns = yamls.__next__()
+    ns_name = IDENTIFIER_PREFIX + "ns-" + i
     ns = yamls[0]
-    ns['metadata']['name'] += i
+    ns['metadata']['name'] = ns_name
     # ns.dump(f"ns-{i}.yaml")
 
     # snat = yamls.__next__()
+    snat_name = IDENTIFIER_PREFIX + "snatpolicy-" + i
+    snat_ip = IP_PREFIX + i
     snat = yamls[1]
-    snat['metadata']['name'] += i
-    snat['spec']['selector']['namespace'] += i
-    snat['spec']['snatIp'][0] += i
+    snat['metadata']['name'] = snat_name
+    snat['spec']['selector']['namespace'] = ns_name
+    snat['spec']['snatIp'][0] = snat_ip
 
     # deploy = yamls.__next__()
+    deploy_name = IDENTIFIER_PREFIX + "ngnix-deployment-" + i
+    selector_name = IDENTIFIER_PREFIX + "resource-" + i
     deploy = yamls[2]
-    deploy['metadata']['name'] += i
-    deploy['metadata']['namespace'] += i
+    deploy['metadata']['name'] = deploy_name
+    deploy['metadata']['namespace'] = ns_name
     deploy['spec']['replicas'] = REPLICAS
-    deploy['spec']['selector']['matchLabels']['name'] += i
-    deploy['spec']['template']['metadata']['labels']['name'] += i
+    deploy['spec']['selector']['matchLabels']['name'] = selector_name
+    deploy['spec']['template']['metadata']['labels']['name'] = selector_name
 
     # svc = yamls.__next__()
+    svc_name = IDENTIFIER_PREFIX + "nginx-svc-" + i
     svc = yamls[3]
-    svc['metadata']['name'] += i
-    svc['metadata']['namespace'] += i
-    svc['spec']['selector']['name'] += i
+    svc['metadata']['name'] = svc_name
+    svc['metadata']['namespace'] = ns_name
+    svc['spec']['selector']['name'] = selector_name
 
     out.extend(yamls)
     # fout = os.path.join(OUTPUT_PATH, f"resources-{i}.yaml")
 print(OUTPUT_FILE)
 with (open(OUTPUT_FILE,'w')) as f:
     yaml.dump_all(out, f)
-
