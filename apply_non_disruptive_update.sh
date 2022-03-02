de4c5429c95b4bec6886013116aa9f79c6cfb895
Mridul Gain <78899625+mridulgain-oc@users.noreply.github.com>
2022-03-02 11:10:44 +0530

    apply the container image change non-disruptively
    Automating the "Nondisruptively Upgrading the Cisco ACI CNI Plug-in" section from [here](https://www.cisco.com/c/en/us/td/docs/switches/datacenter/aci/apic/sw/4-x/installation-upgrade-downgrade/Cisco-ACI-CNI-Plug-in-Upgrade.html#id_99051)

--- apply_non_disruptive_update.sh
+++ apply_non_disruptive_update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-for node in $(oc get node -o name)
+for node in $(kubectl get node -o name)
 do
     node=($(echo $node | tr "/" " "))
     node=${node[1]}
-    oc adm drain $node --force --delete-emptydir-data --ignore-daemonsets
-    for i in $(oc -n aci-containers-system get pod -o wide | grep $node | awk '{print $1}'); do
-        oc -n aci-containers-system delete po $i
+    #oc adm drain $node --force --delete-emptydir-data --ignore-daemonsets
+    kubectl drain $node --force --delete-emptydir-data --ignore-daemonsets
+    for i in $(kubectl -n aci-containers-system get pod -o wide | grep $node | awk '{print $1}'); do
+        kubectl -n aci-containers-system delete po $i
     done
     sleep 5
     kubectl uncordon $node
